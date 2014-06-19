cf910feab7bc7a5bf65418d67664669ebb445561
Rafael Garcia <rafbgarcia@gmail.com>
2014-06-18 23:13:55 -0300

    Create topics for communities

--- config/routes.js
+++ config/routes.js
@@ -3,14 +3,8 @@
 
   angular.module('Routes')
 
-  .constant('states', [
-    'user_edit',
-    'communities_index', // { url: '/communities', controller: 'CommunitiesIndex', templateUrl: '/communities/index.tmpl.html' }
-    'communities_show'
-  ])
-
-  .config(['$stateProvider', '$urlRouterProvider', 'states',
-    function ($stateProvider, $urlRouterProvider, states) {
+  .config(['$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
       $urlRouterProvider.otherwise("/");
 
       $stateProvider
@@ -18,46 +12,42 @@
           url: "/",
           templateUrl: 'home/index.tmpl.html'
         })
-        .state('logout', {
-          url: "/logout",
-          controller: function (User, $state, $rootScope) {
-            User.logOut();
-            $rootScope.user = undefined;
-            $state.go('root');
-          }
-        });
 
-      states.forEach(function (state) {
-        $stateProvider.state(state, {
-          url: urlForState(state),
-          controller: controllerForState(state),
-          templateUrl: templateForState(state)
-        });
+      // User
+      .state('user_edit', {
+        url: '/user/edit',
+        controller: 'UserEdit',
+        templateUrl: 'user/edit.tmpl.html'
+      })
+
+      // Communities
+      .state('communities_index', {
+        url: '/communities/index',
+        controller: 'CommunitiesIndex',
+        templateUrl: 'communities/index.tmpl.html'
+      })
+        .state('communities_show', {
+          url: '/communities/:id',
+          controller: 'CommunitiesShow',
+          templateUrl: 'communities/show.tmpl.html'
+        })
+
+      // Topics
+      .state('communities_topics_new', {
+        url: '/communities/:id/topics/new',
+        controller: 'TopicsNew',
+        templateUrl: 'topics/new.tmpl.html'
+      })
+
+
+      .state('logout', {
+        url: "/logout",
+        controller: function (User, $state, $rootScope) {
+          User.logOut();
+          $rootScope.user = undefined;
+          $state.go('root');
+        }
       });
-
-      function urlForState(state) {
-        var pieces = state.split('_');
-        var path = pieces.join('/');
-
-        path = path.replace(/\/index$/, '')
-          .replace(/show$/, ':id');
-
-        return '/' + path;
-      }
-
-      function controllerForState(state) {
-        var controller = '';
-        state.split('_').forEach(function (piece) {
-          controller += piece[0].toUpperCase() + piece.substring(1, piece.length);
-        });
-        return controller;
-      }
-
-      function templateForState(state) {
-        var pieces = state.split('_');
-        var path = pieces.join('/');
-        return path + '.tmpl.html';
-      }
     }
   ]).run(['$state', _.noop]); // re-evaluates routes when page is reloaded
 }());
