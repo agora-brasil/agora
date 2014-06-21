1509627c654a8751ddbe5acbef980452ce6de1c2
Rafael Garcia <rafbgarcia@gmail.com>
2014-06-21 14:16:57 -0300

    Format routes

--- config/routes.js
+++ config/routes.js
@@ -8,41 +8,18 @@
       $urlRouterProvider.otherwise("/");
 
       $stateProvider
-        .state('root', {
-          url: "/",
-          templateUrl: 'home/index.tmpl.html'
-        })
+      .state('root', {url: "/", templateUrl: 'home/index.tmpl.html'})
 
       // User
-      .state('user_edit', {
-        url: '/user/edit',
-        controller: 'UserEdit',
-        templateUrl: 'user/edit.tmpl.html'
-      })
+      .state('user_edit', {url: '/user/edit', controller: 'UserEdit', templateUrl: 'user/edit.tmpl.html'})
 
       // Communities
-      .state('communities_index', {
-        url: '/communities',
-        controller: 'CommunitiesIndex',
-        templateUrl: 'communities/index.tmpl.html'
-      })
-        .state('communities_show', {
-          url: '/communities/:id',
-          controller: 'CommunitiesShow',
-          templateUrl: 'communities/show.tmpl.html'
-        })
+      .state('communities_index', {url: '/communities', controller: 'CommunitiesIndex', templateUrl: 'communities/index.tmpl.html'})
+      .state('communities_show', {url: '/communities/:id', controller: 'CommunitiesShow', templateUrl: 'communities/show.tmpl.html'})
 
       // Topics
-      .state('communities_topics_new', {
-        url: '/communities/:id/topics/new',
-        controller: 'TopicsNew',
-        templateUrl: 'topics/new.tmpl.html'
-      })
-        .state('communities_topics_show', {
-          url: '/communities/:id/topics/:topic_id',
-          controller: 'TopicsShow',
-          templateUrl: 'topics/show.tmpl.html'
-        })
+      .state('communities_topics_new', {url: '/communities/:id/topics/new', controller: 'TopicsNew', templateUrl: 'topics/new.tmpl.html'})
+      .state('communities_topics_show', {url: '/communities/:id/topics/:topic_id', controller: 'TopicsShow', templateUrl: 'topics/show.tmpl.html'})
 
 
       .state('logout', {
